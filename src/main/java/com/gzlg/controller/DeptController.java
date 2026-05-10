79f013b0a85ad8c2db7c77ff8bf0115e2564aaa7
fengyou-tianzi <fengyou-tianzi@users.noreply.github.com>
2026-05-10 16:08:41 +0000

    feat: 生成带注释的Logback配置
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/main/java/com/gzlg/controller/DeptController.java
+++ src/main/java/com/gzlg/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.gzlg.controller;
 import com.gzlg.pojo.Dept;
 import com.gzlg.pojo.Result;
 import com.gzlg.service.DeptService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import java.util.List;
 @RestController
 public class DeptController {
 
+    private static final Logger log = LoggerFactory.getLogger(DeptController.class);
+
     @Autowired
     private DeptService deptService;
 
@@ -20,7 +24,9 @@ public class DeptController {
      */
     @GetMapping("/depts")
     public Result findAll() {
+        log.info("收到查询所有部门的请求");
         List<Dept> deptList = deptService.findAll();
+        log.info("查询完成，共查询到 {} 条部门数据", deptList.size());
         return Result.success(deptList);
     }
 
